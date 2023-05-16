5f65bcb313c91d16e895d6571dc9293186153fa8
jooji-san <g.shengelaia2001@gmail.com>
2023-05-16 13:25:01 +0400

    dial stuff

--- dial.py
+++ dial.py
@@ -4,18 +4,22 @@ from datetime import datetime, timedelta
 
 def get_dial_input():
   num_list = []
-  timeout = timedelta(seconds=10)
+  timeout = timedelta(seconds=3)
   first_time = True
   end = False
   while True:
+    servo.detach()
     time_start = datetime.now()
     time_end = time_start + timeout
-    while (servo_feedback.value < 0.1): # nearly zero
+    print("this is value " + str(servo_feedback.value))
+    while (servo_feedback.value < 0.25): # minimum value
       print(servo_feedback.value)
-      if (not first_time and time.now < time_end):
+      if (not first_time and datetime.now() > time_end):
+        print("ending")
         end = True
         break
-      # wait for 0.01s
+      time.sleep(0.01)
+
     print('hihi')
     if (end):
       if (len(num_list) == 0):
@@ -27,16 +31,18 @@ def get_dial_input():
     
     first_time = False
     prev = servo_feedback.value
-    print("0 sec")
-    time.sleep(1)
-    print("1 sec")
-    while (prev != servo_feedback.value):
+    time.sleep(2)
+    while (abs(prev - servo_feedback.value) > 0.05):
+      print("hey")
       prev = servo_feedback.value
-      time.sleep(1)
-    
+      time.sleep(2)
+
+    print(prev - servo_feedback.value) 
     num = to_num(servo_feedback.value)
     num_list.append(num)
+    print(num_list)
     servo.min()
+    time.sleep(1)
 
 def to_num(input):
   if (input > 0 and input < 0.5):
@@ -44,7 +50,14 @@ def to_num(input):
   else: 
     return 0
   
-servo = Servo(25)
+myGPIO=25
+ 
+maxPW=(2.0+0.25)/1000
+minPW=(1.0-0.55)/1000
+ 
+servo = Servo(myGPIO,min_pulse_width=minPW,max_pulse_width=maxPW)
 servo.min()
+print("min")
+time.sleep(2)
 servo_feedback = MCP3004(channel=0)
-get_dial_input()
+print(get_dial_input())
